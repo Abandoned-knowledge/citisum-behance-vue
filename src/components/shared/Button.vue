973e801b83e9971e2e8ec7c87be6e5209fb87bbd
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-05-24 17:39:42 +0300

    create Button.vue

---
+++ src/components/shared/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  @apply rounded-full border border-dark bg-dark px-8 py-4 text-xl text-light transition duration-200;
+
+  &:hover {
+    @apply bg-opacity-0 text-dark;
+  }
+}
+</style>
