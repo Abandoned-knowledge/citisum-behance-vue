412aa08c17c18c8adc64a8fa5f0270125ee866b6
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-05-24 14:20:20 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue, js, ts}"],
+  theme: {
+    colors: {
+      dark: "#1D1D1D",
+      light: "#FAFCFD",
+      "light-darked": "#F3F3F3",
+      grey: "#707173",
+      accent: "#FF5C00",
+      extra: "#FFC95C",
+      links: "#4550C2",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
