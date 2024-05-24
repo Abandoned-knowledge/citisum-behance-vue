2381aa14b3097a955b99d5979dc30bbed3e72e8f
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-05-24 17:41:47 +0300

    create Header.vue

---
+++ src/components/blocks/Header.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import Button from "../shared/Button.vue";
+import LogoIcon from "../icons/LogoIcon.vue";
+import type { menuItem } from "@/interfaces/interfaces";
+
+const menuItems: menuItem[] = [
+  {
+    title: "About Us",
+    link: "#",
+  },
+  {
+    title: "Project",
+    link: "#",
+  },
+  {
+    title: "Agents",
+    link: "#",
+  },
+  {
+    title: "Services",
+    link: "#",
+  },
+];
+</script>
+
+<template>
+  <header class="container mx-auto flex items-center justify-between pt-5">
+    <div class="flex items-center gap-5">
+      <LogoIcon class="text-dark" />
+      <span class="text-4xl font-medium">Citisum</span>
+    </div>
+
+    <nav>
+      <ul class="flex gap-16">
+        <li v-for="item in menuItems" class="link">
+          <a :href="item.link">{{ item.title }}</a>
+        </li>
+      </ul>
+    </nav>
+
+    <Button> Contact Us </Button>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.link {
+  @apply text-dark;
+  position: relative;
+
+  &:hover {
+    &::before {
+      width: 100%;
+    }
+  }
+
+  &:before {
+    @apply bg-dark;
+    transition: all 0.3s ease;
+    content: "";
+    position: absolute;
+    height: 1px;
+    top: 100%;
+    width: 0;
+  }
+}
+</style>
---
+++ src/components/icons/LogoIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M21.75 36.25V11.257L30.6029 6.28181V36.25H21.75Z" stroke="currentColor" stroke-width="1.5" />
+    <path
+      d="M36.25 36.25V11.1223L32.1029 6.84958V36.25H36.25Z"
+      fill="currentColor"
+      stroke="currentColor"
+      stroke-width="1.5"
+    />
+    <path d="M5.75 36.25V21.3545L14.6029 17.1825V36.25H5.75Z" stroke="currentColor" stroke-width="1.5" />
+    <path
+      d="M20.25 36.25V21.222L16.1029 17.6391V36.25H20.25Z"
+      fill="currentColor"
+      stroke="currentColor"
+      stroke-width="1.5"
+    />
+  </svg>
+</template>
