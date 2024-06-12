95695c0a55979fb19e74b8653d7e1afaf83b2727
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-12 10:32:35 +0300

    fix: change padding and font-size in Button.vue

--- src/components/shared/Button.vue
+++ src/components/shared/Button.vue
@@ -6,7 +6,7 @@
 
 <style lang="scss" scoped>
 .button {
-  @apply w-fit h-fit rounded-full border border-dark bg-dark px-8 py-4 text-light transition duration-200;
+  @apply h-fit w-fit rounded-full border border-dark bg-dark px-12 py-5 text-light transition duration-200;
 
   &:hover {
     @apply bg-opacity-0 text-dark;
