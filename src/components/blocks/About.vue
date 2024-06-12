63a8c2dd1317ea687642da7de94d11804efb0fc5
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-12 10:54:50 +0300

    fix: add responsive to About.vue

--- src/components/blocks/About.vue
+++ src/components/blocks/About.vue
@@ -3,22 +3,22 @@ import { imageLinks, aboutBenefits } from "@/data/about";
 </script>
 
 <template>
-  <div class="h-screen bg-dark m-comp">
-    <section class="container mx-auto flex h-full items-center gap-20">
-      <div class="images grid w-[50%]">
+  <div class="m-comp flex min-h-screen items-center justify-center bg-dark">
+    <section class="container mx-auto flex h-full flex-col items-center justify-center gap-20 py-10 md:flex-row">
+      <div class="images grid w-[96%] md:w-[50%]">
         <img v-for="item in imageLinks" :src="item.link" alt="" :class="`images__item images__item_${item.count}`" />
-        <div class="flex flex-col items-center justify-center gap-4 p-5 text-accent">
+        <div class="hidden flex-col items-center justify-center gap-4 p-5 text-accent md:flex">
           <h1 class="h1-text">23+</h1>
           <p class="title-text">Over Experience</p>
         </div>
       </div>
 
-      <div class="flex w-[50%] flex-col gap-10">
+      <div class="-order-1 md:order-1 flex w-[96%] flex-col items-center gap-5 md:w-[50%] md:items-start md:gap-10">
         <p class="body-text body-text_uppercase text-light">dream house our process</p>
-        <h2 class="h2-text text-light">our work process make your dream home</h2>
+        <h2 class="h2-text text-center text-light md:text-start">our work process make your dream home</h2>
         <div class="flex w-[80%] flex-col gap-7">
           <article v-for="benefit in aboutBenefits" class="flex gap-5">
-            <component :is="benefit.icon" class="icon row-span-2 h-16 w-16" />
+            <component :is="benefit.icon" class="icon row-span-2 h-12 w-12 md:h-16 md:w-16" />
             <div class="flex flex-col gap-2">
               <p class="title-text text-light">{{ benefit.title }}</p>
               <p class="body-text text-light">{{ benefit.content }}</p>
@@ -39,14 +39,20 @@ import { imageLinks, aboutBenefits } from "@/data/about";
 }
 
 .images {
-  @apply grid h-[70%] grid-cols-2 gap-6;
+  @apply grid grid-cols-3 gap-6 md:h-[70%] md:grid-cols-2;
+  @media screen and (max-width: 500px) {
+    @apply grid-cols-2;
+  }
   &__item {
     @apply h-full w-full rounded-[15px] object-cover;
     &_2 {
-      grid-row: span 2;
+      @apply md:row-span-2;
     }
     &_3 {
-      grid-row: span 2;
+      @apply md:row-span-2;
+      @media screen and (max-width: 500px) {
+        @apply col-span-2;
+      }
     }
   }
 }
